9e76f4a27e66be9994649a94bb234183c7efcca6
rustrar <rustrar@gmail.com>
2021-03-18 20:10:29 +0400

    Выполнил задание второй лекции

---
+++ dotNet module/Lection_2/Task_1/IRemind.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+  public interface IRemind
+  {
+    public DateTime ReminderDateTime { get; set; }
+
+    public void SetRemindDateTime(DateTime reminderDateTime);    
+
+    public DateTime GetRemindDateTime();
+  }
+}
