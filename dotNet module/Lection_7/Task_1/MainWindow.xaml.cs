3fd142e09171c9e898c3b9827087ffc08e2f09f1
rustrar <rustrar@gmail.com>
2021-03-21 18:33:54 +0400

    Задание к занятию 7

---
+++ dotNet module/Lection_7/Task_1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Task_1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      LoadFromGzip("q2.rtf.gz");
+    }
+
+    private void LoadFromGzip(string filePath)
+    {
+      TextRange doc = new TextRange(textBox.Document.ContentStart, textBox.Document.ContentEnd);
+      using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+        using (GZipStream stream = new GZipStream(file, CompressionMode.Decompress))
+        {
+          doc.Load(stream, DataFormats.Rtf);
+        }
+    }
+  }
+}
